29dfa63b0dd7efa15bc9677e5dfd02bfd552a7ca
swady99 <69527003+swady99@users.noreply.github.com>
2021-06-25 12:51:43 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<math.h>
+
+    int main()
+{
+    int i,j;
+    int o=1,z=0;
+    int arr1[3],arr2[3];
+    printf("Enter the elements in arr1: ");
+    for(i=0;i<3;i++)
+    {
+        scanf("%d ",&arr1[i]);
+    }
+    printf("Enter the elements in arr2: ");
+    for(j=0;j<3;j++)
+    {
+        scanf("%d ",&arr[j]);
+    }
+for(i=0;i<3;i++)
+{
+    {
+        if(arr1[i]==1)
+        {
+            arr1[i]=z;
+        }
+        else
+        {
+            arr1[i]=o;
+        }
+    }
+}
+for(i=0;i<3;i++)
+{
+    printf("%d ",arr1[i]);
+}
+}
